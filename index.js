1c6da25cafeadebd839a6369430913d1a708d14d
brandongatlin <brandongatlin1981@me.com>
2019-08-01 18:52:58 -0500

    added cert token and route

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ app.get("/", function(req, res) {
 app.listen(PORT, function() {
   console.log(`server taking requests on port: ${PORT}`);
 });
+
+app.get(
+  "/.well-known/acme-challenge/mQFyvUi9ZFlnnd2tBHIYVvd5ptPJ43TJ-fS9lsRTc54",
+  function(req, res) {
+    res.send(`mQFyvUi9ZFlnnd2tBHIYVvd5ptPJ43TJ-fS9lsRTc54.eOVjY1YlTO0ZoomXCmZaODqkEo9YuO-jgGPb1UscD5M
+`);
+  }
+);
